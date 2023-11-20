94fc51554670283ea290a4e8163b66c92bd9d134
gebre-a <kidanalemayehu12@gmail.com>
2023-11-20 04:13:04 -0800

    vh

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,18 +1,14 @@
-
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    ele = 0  # Number of elements
+    if my_list is None:
+        return 0
+    count = 0
     try:
-        if x == 0:
-            print()
-            return x
-        for n in range(0, x):
-            if my_list[n] != my_list[-1] and n != (x - 1):
-                print(my_list[n], end="")
-                ele += 1
-            else:
-                print(my_list[n])  # Add new line to the last element
-                ele += 1
-        return ele
-    except BaseException:
-        return ele
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            count = count + 1
+        print()
+    except IndexError:
+        print()
+        pass
+    return count
