fdbb9bbb3dc02152b2b08839840c1e9946dde4bc
gebre-a <kidanalemayehu12@gmail.com>
2023-11-27 02:58:02 -0800

    task 101

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -5,9 +5,6 @@
 Determines all possible solutions to placing N
 N non-attacking queens on an NxN chessboard.
 
-Example:
-    $ ./101-nqueens.py N
-
 N must be an integer greater than or equal to 4.
 
 Attributes:
