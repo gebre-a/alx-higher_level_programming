656604277159b16e8fd5b836cce081f43169a71c
gebre-a <kidanalemayehu12@gmail.com>
2023-12-05 22:02:53 -0800

    task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module containing the function append_write"""
+
+
+def append_write(filename="", text=""):
+    """Appends a string to a text file (UTF8) and returns the number
+    of characters added.
+     """
+    with open(filename, 'a', encoding="utf-8") as f:
+        """This method returns the number of characters appended to a file."""
+        return f.write(text)
