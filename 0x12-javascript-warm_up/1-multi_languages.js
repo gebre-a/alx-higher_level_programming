437bcf2d39ad317804e832587302e222c3b31019
gebre-a <kidanalemayehu12@gmail.com>
2024-02-12 22:16:34 -0800

    task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-
-const phrase1 = 'C is fun';
-const phrase2 = 'Python is cool';
-const phrase3 = 'Javascript is amazing';
-
-console.log(phrase1);
-console.log(phrase2);
-console.log(phrase3);
+['C is fun', 'Python is cool', 'JavaScript is amazing'].forEach(
+  elem => console.log(elem)
+);
