f167f4acdbea259783a22fa008f691bea19f4282
gebre-a <kidanalemayehu12@gmail.com>
2024-03-01 06:12:25 -0800

    task 3

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 """
-takes in a URL and an email, sends a POST request to the passed URL with
- the email as a parameter, and displays the body
- of the response (decoded in utf-8)
+Script that takes in a URL, sends a request to the URL and displays the
+body of the response (decoded in utf-8).
+
+Usage: ./3-error_code.py <URL>
+  - Handles HTTP errors.
 """
-import sys
+from sys import argv
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
 
-import urllib.request
 
-if __name__ == '__main__':
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = url.parse.urlencode(value).encode("ascii")
+if __name__ == "__main__":
+    url = argv[1]
+    req = Request(url)
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    try:
+        with urlopen(req) as response:
+            print(response.read().decode("ascii"))
+    except HTTPError as e:
+        print("Error code: {}".format(e.code))
