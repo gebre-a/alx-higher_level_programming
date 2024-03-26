bdede312043b631c580264f168b911fdd6002710
gebre-a <kidanalemayehu12@gmail.com>
2024-03-26 04:09:22 -0700

    js web scrapping

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+const id = process.argv[2];
+
+request
+  .get(`https://swapi-api.alx-tools.com/api/films/${id}`, (err, res, body) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const data = JSON.parse(body);
+      console.log(data.title);
+    }
+  });
